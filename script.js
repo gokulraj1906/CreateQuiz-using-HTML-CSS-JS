82ee7f8c97897d8966c275a6a73193c43c569f16
gokulraj1906 <141732593+gokulraj1906@users.noreply.github.com>
2024-07-22 16:45:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function option1(){
+    document.getElementById("opt1").style="background-color: red;color: white";
+    document.getElementById("ans1").style="background-color: red; color: white";
+}
+function option2(){
+    document.getElementById("opt2").style="background-color: red;color: white";
+    document.getElementById("ans2").style="background-color: red; color: white";
+}
+function option3(){
+    document.getElementById("opt3").style="background-color: red;color: white";
+    document.getElementById("ans3").style="background-color: red; color: white";
+}
+function option4(){
+    document.getElementById("opt4").style="background-color: green;color: white";
+    document.getElementById("ans4").style="background-color: green; color: white";
+}
